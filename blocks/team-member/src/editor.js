ec27ebdc3f4237650da0b778c09ad21bcdc44972
mac <gheorghe@enspiredev.com>
2021-01-30 07:50:02 +0200

    init team member block

---
+++ blocks/team-member/src/editor.js
@@ -0,0 +1,45 @@
+import './editor.scss'
+import { registerBlockType } from '@wordpress/blocks'
+import { __ } from '@wordpress/i18n'
+import { InnerBlocks } from '@wordpress/block-editor'
+import Edit from './edit'
+
+const attributes = {
+  maxHeight: {
+    type: 'number',
+    default: 215,
+  },
+}
+
+registerBlockType('gz/team-member', {
+  title: __('Team Member', 'gz-blocks'),
+  category: 'layout',
+  icon: {
+    background: '#004696',
+    foreground: '#fff',
+    src: 'arrow-down-alt2',
+  },
+  keywords: [
+    __('team', 'gz-blocks'),
+    __('user card', 'gz-blocks'),
+    __('member', 'gz-blocks'),
+  ],
+  attributes,
+  edit: Edit,
+  save: function ({ attributes }) {
+    const { maxHeight } = attributes
+
+    return (
+      <div>
+        <div className="seemore">
+          <div className="max-height" style={{ maxHeight: maxHeight }}>
+            <div className="full-text">
+              <InnerBlocks.Content/>
+            </div>
+          </div>
+          <div className="btn-more"></div>
+        </div>
+      </div>
+    )
+  },
+})
---
+++ blocks/team-member/team-member.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Team Member Block
+ */
+
+function gz_register_team_member() {
+
+	// Register our block editor-specific front css
+	wp_register_style(
+		'gz-team-member',
+		plugins_url( 'dist/style.css', __FILE__ ),
+		array( 'wp-blocks' )
+	);
+
+	// Register our block editor-specific front js
+	wp_register_script(
+		'gz-team-member',
+		plugins_url( 'dist/script.js', __FILE__ ),
+		array( 'jquery' )
+	);
+
+	// Register our block editor-specific CSS/JS
+	if ( is_admin() ) :
+		wp_register_style(
+			'gz-team-member-editor',
+			plugins_url( 'dist/editor.css', __FILE__ ),
+			array( 'wp-edit-blocks' )
+		);
+
+		// Register our block script with WordPress
+		wp_register_script(
+			'gz-team-member-editor',
+			plugins_url( 'dist/editor.js', __FILE__ ),
+			array( 'wp-blocks', 'wp-element', 'wp-editor' )
+		);
+	endif;
+
+	// Enqueue the script in the editor
+	register_block_type( 'gz/team-member', array(
+		'editor_script' => 'team-member-editor',
+		'editor_style'  => 'team-member-editor',
+		'style'         => 'team-member',
+		'script'        => 'team-member',
+	) );
+}
+
+add_action( 'init', 'gz_register_team_member' );
